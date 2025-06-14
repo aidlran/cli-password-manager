f8b3309222f54751e0126a57cf66341d1badab21
Aidan <86127892+aidlran@users.noreply.github.com>
2025-06-14 22:25:09 +0100

    feat: migrate

--- src/migrate.mjs
+++ src/migrate.mjs
@@ -1,55 +1,85 @@
-import { clients } from '@astrobase/core/rpc/client';
-import sqliteLegacy from '@astrobase/core/sqlite';
+import { decodeWithCodec, getContent } from '@astrobase/core';
 
 import wordlist from '@astrobase/sdk/bip39/wordlist/english' with { type: 'json' };
-import { Common } from '@astrobase/sdk/common';
-import { createInstance } from '@astrobase/sdk/instance';
+import { FileBuilder } from '@astrobase/sdk/file';
+import { putIdentity } from '@astrobase/sdk/identity';
+import { putImmutable } from '@astrobase/sdk/immutable';
 import { createKeyring, getAvailableKeyringCIDs, loadKeyring } from '@astrobase/sdk/keyrings';
-import sqlite from '@astrobase/sdk/sqlite';
 
-import { inspect } from 'util';
+import { cpSync } from 'fs';
 
+import pkg from '../package.json' with { type: 'json' };
+
+import { decrypt, encrypt } from './lib/crypt.mjs';
 import { getIndex } from './lib/legacy/index-file.mjs';
-import { prompt } from './lib/readline.mjs';
 
 export async function migrate(
   /** @type {import('@astrobase/sdk/instance').Instance} */ instance,
   /** @type {string} */ passphrase,
+  /** @type {string} */ dbPath,
 ) {
   // If keyring exists then migration has happened
-  const availableKeyringCIDs = await getAvailableKeyringCIDs(instance);
-  console.log(inspect({ availableKeyringCIDs }));
-  if (availableKeyringCIDs.length) {
-    console.log('Skipping migration...');
+  if ((await getAvailableKeyringCIDs(instance)).length) {
     return;
   }
 
-  // TODO: get original index with old API - if decrypt fails, we have the wrong passphrase
+  // Get original index with old API - if decrypt fails, we have the wrong passphrase
   const index = await getIndex(passphrase, () => {
     console.error('Incorrect database passphrase');
     process.exit(1);
   });
 
-  console.log('Beginning migration...');
+  const backupPath = `${dbPath}.bak`;
+  cpSync(dbPath, backupPath);
+
+  console.log(`Backup made at ${backupPath}\nBeginning migration...`);
 
   // Create keyring using passphrase
   const { cid } = await createKeyring(instance, { passphrase, wordlist });
   await loadKeyring(instance, { cid, passphrase, wordlist });
-  console.log('Keyring created and loaded.');
 
-  for (const [id, { added, cid }] of Object.entries(index)) {
-    // TODO: recreate, with new file format and crypt key, including parent chain
-    // TODO: update cid pointer
+  // Update each entry in the index
+  for (const [id, { cid }] of Object.entries(index)) {
+    /** @type {import('./cli.mjs').Entry['props'][]} */
+    const stack = [];
+
+    let prev = cid;
+
+    // Push each generation onto stack (oldest on top)
+    do {
+      const entry = await decodeWithCodec(
+        decrypt((await getContent(prev)).payload, passphrase, () =>
+          program.error('Incorrect database passphrase'),
+        ),
+        'application/json',
+      );
+      stack.push(entry.props);
+      prev = entry.prev;
+    } while (prev);
+
+    // Pop throughout the stack to build a new chain
+    while (stack.length) {
+      prev = await putImmutable(
+        new FileBuilder().setPayload(
+          await encrypt({ prev, props: stack.pop() }, passphrase, instance),
+        ),
+        { instance },
+      );
+    }
+
+    // Update entry's CID in index
+    index[id].cid = prev;
   }
 
-  // TODO: save new index
+  // Save the new index using identity
+  await putIdentity({
+    id: pkg.name,
+    instance,
+    ref: await putImmutable(
+      new FileBuilder().setPayload(await encrypt(index, passphrase, instance)),
+      { instance },
+    ),
+  });
+
+  console.log('Migration complete.');
 }
-
-const sqliteConfig = { filename: 'test/test-1.db' };
-
-clients.add({ strategy: sqliteLegacy(sqliteConfig) });
-
-migrate(
-  createInstance(Common, { clients: [{ strategy: sqlite(sqliteConfig) }] }),
-  prompt('Enter db passphrase'),
-);
